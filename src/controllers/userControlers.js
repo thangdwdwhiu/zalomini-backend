6cba86c4f25ed7af7c7fcf53971df4ed9e942a1a
Xuan Thang <thangthitotnghiep@gmail.com>
2025-11-12 13:48:48 +0700

    update cookie

--- src/controllers/userControlers.js
+++ src/controllers/userControlers.js
@@ -6,13 +6,14 @@ const login = async (req, res) => {
     try{
     const {user_id, username, password, phone, avatar, fullname} = await userServices.login(req)
     const token = generateJWT(user_id,fullname, username);
-    res.cookie('jwt', token, {
-    secure: false,
-    httpOnly: true,
-    path: '/',
-    maxAge: 1000*60*60*2,
-    sameSite: 'lax'
-  } )
+res.cookie('jwt', token, {
+  secure: true,           // bắt buộc HTTPS
+  httpOnly: true,         // JS frontend không đọc được
+  path: '/',
+  maxAge: 1000*60*60*2,   // 2h
+  sameSite: 'None'         // cho phép cross-origin
+});
+
     res.status(200).json({success: true, message: 'dang nhap thanh cong', data: {user_id, username, password, phone, avatar, fullname }})
     }
     catch (e){
